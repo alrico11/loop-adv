3aef4f2afa346f02462bb66bcd568d5be5b2b3f4
Alrico Rizki <alricosaiyan123@gmail.com>
2023-03-25 19:23:55 +0700

    TAKE HOME TASK

--- task.js
+++ task.js
@@ -21,7 +21,7 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for (let i=0;i<=arr.length;i++){
     for(let j=0;j<arr.length;j++){
         if(arr[i]+arr[j]==17){
-            console.log(`${arr[i]} + ${arr[j]}`)
+            console.log(`${arr[i]} + ${arr[j]} = ${arr[i] + arr[j]}`)
         }
     }
 }
